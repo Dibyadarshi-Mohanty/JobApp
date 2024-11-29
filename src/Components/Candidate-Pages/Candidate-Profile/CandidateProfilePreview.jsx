e95cb9070de0001be6fbfd0247053ae32de07c1c
Aditya <adityasahoo652@gmail.com>
2024-11-29 11:39:43 +0530

    profile and logout added

--- src/Components/Candidate-Pages/Candidate-Profile/CandidateProfilePreview.jsx
+++ src/Components/Candidate-Pages/Candidate-Profile/CandidateProfilePreview.jsx
@@ -1,37 +1,48 @@
-import React from 'react'
+import { useDispatch, useSelector } from "react-redux"
 import "./CandidateProfilePreview.css"
+import { logout } from "../../../redux/actions/user";
+
 export default function CandidateProfilePreview() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+
+  const handleLogout = () => {
+    dispatch(logout("candidate"));
+  }
   return (
     <>
-    
-    <div className='container preview-div'>
-      <div className="profile-card">
-      <h1 className='preview-h1'>Profile Preview</h1>
-        <img className="profile-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="Profile Picture"/>
-      
-        <div className="profile-details">
-            <label>Name:</label>
-            <p>Kshiti Ghelani</p>
-            
-            <label>Email:</label>
-            <p>kshitighelani@gmail.com</p>
-            
-            <label>Phone:</label>
-            <p>123 456 7890</p>
-            
-            <label>Domain:</label>
-            <p>Web Development</p>
-            
-            <label>Skills:</label>
-            <p>HTML, CSS, JavaScript, React</p>
-        </div>
 
-        <div className="profile-buttons">
+      <div className='container preview-div'>
+        <div className="profile-card">
+          <img className="profile-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="Profile Picture" />
+
+          <div className="profile-details">
+            <label>Name:</label>
+            <p>
+              {user?.name}
+            </p>
+
+            <label>Email:</label>
+            <p>{user?.email}</p>
+
+            <label>Phone:</label>
+            <p>{user?.phoneNumber ? user?.phoneNumber : "Not added"}</p>
+
+            <label>Domain:</label>
+            <p>{user?.areaOfStudy ? user?.areaOfStudy : "Not added"}</p>
+
+            <label>Skills:</label>
+            <p>
+              {user?.skills.length > 0 ? user?.skills.join(",") : "Not added"}
+            </p>
+          </div>
+
+          <div className="profile-buttons">
             <button className="btn edit">Edit Profile</button>
-            <button className="btn logout">Logout</button>
+            <button className="btn logout" onClick={handleLogout}>Logout</button>
+          </div>
         </div>
-    </div>
-    </div>
+      </div>
     </>
   )
 }
